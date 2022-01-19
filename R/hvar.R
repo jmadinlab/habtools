a29133a238d9754c390c22a79e6a26a4d304321a
nschiett <nina.schiettekatte@gmail.com>
2022-01-18 15:16:04 -1000

    trying to speed up hvar

--- R/hvar.R
+++ R/hvar.R
@@ -12,19 +12,33 @@
 #' @details
 #' This function is used for calculating fractal dimension using the
 #' height variation method.
-#' It is separated from the `FD` function because it takes a while to run, and so it is useful
-#' to save the output before using the `FD` function.
+#' It is separated from the `fd` function because it takes a while to run, and so it is useful
+#' to save the output before using the `fd` function.
 #'
 #' @examples
 #'
 #' hvar(horseshoe, x=-470, y=1266, L=2, Lvec=c(2, 1, 0.5))
 
-hvar <- function(dem, x, y, L, Lvec) {
-  hvar <- data.frame()
-  for (L0 in Lvec) {
-    cells <- expand.grid(x=seq(x, x+L-L0, L0), y=seq(y, y+L-L0, L0))
-    H0 <- mapply(hr, x=cells$x, y=cells$y, MoreArgs=list(L=L0, data=dem))
-    hvar <- rbind(hvar, data.frame(L0=L0, H0=H0))
+hvar <- function(dem, x, y, L, Lvec,
+                 parallel = FALSE,
+                 ncores = (parallel::detectCores()-1)) {
+  if (parallel == FALSE){
+    hvar <-
+      lapply(Lvec, function(L0){
+        cells <- expand.grid(x=seq(x, x+L-L0, L0), y=seq(y, y+L-L0, L0))
+        H0 <- mapply(hr, x=cells$x, y=cells$y, MoreArgs=list(L=L0, data=dem))
+        data.frame(L0=L0, H0=H0)
+      }) %>% dplyr::bind_rows()
+  } else if (parallel == T) {
+    hvar <-
+      lapply(Lvec, function(L0){
+        cells <- expand.grid(x = seq(x, x + L - L0, L0),
+                             y = seq(y, y + L - L0, L0))
+        H0 <- parallel::mclapply(1:nrow(cells), function(i){
+          hr(x = cells[i, "x"], y = cells[i, "y"], L = L0, data = dem)
+        }, mc.cores = ncores) %>% simplify2array()
+        data.frame(L0=L0, H0=H0)
+      }) %>% dplyr::bind_rows()
   }
   return(hvar)
 }
