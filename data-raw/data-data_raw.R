b2cbcb9efa08da89f155404073b33e82cb2fb116
nschiett <nina.schiettekatte@gmail.com>
2022-08-24 09:20:34 -1000

    merge

--- data-raw/data-data_raw.R
+++ data-raw/data-data_raw.R
@@ -1,4 +1,5 @@
 ## code to prepare `data-raw/horseshoe.tif` dataset goes here
 
 horseshoe <- raster::raster("data-raw/horseshoe.tif")
+horseshoe <- raster::readAll(horseshoe)
 usethis::use_data(horseshoe, overwrite = TRUE)
