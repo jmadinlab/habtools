af546edd08dddd4ac595e593421145a2b9da6851
nschiett <nina.schiettekatte@gmail.com>
2022-09-02 12:00:56 -1000

    data file decreased in size

--- data-raw/data-data_raw.R
+++ data-raw/data-data_raw.R
@@ -1,6 +1,8 @@
 ## code to prepare `data-raw/horseshoe.tif` dataset goes here
 
 horseshoe <- raster::raster("data-raw/horseshoe.tif")
-horseshoe <- raster::readAll(horseshoe)
+res(horseshoe)
+horseshoe <- raster::aggregate(horseshoe, fact = 5)
+res(horseshoe)
 usethis::use_data(horseshoe, overwrite = TRUE)
 
