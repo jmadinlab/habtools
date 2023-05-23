a2ca43a6b52b7eddab0b32ef35a6227273d6b2c2
Joshua Madin <jmadin@hawaii.edu>
2023-05-23 18:08:09 +1000

    Josh

---
+++ R/csf.R
@@ -0,0 +1,29 @@
+#' Calculates colony shape factor of a mesh
+#'
+#' @param mesh mesh object
+#' @param silent Print messages and warnings
+#'
+#' @return A value indicating colony shape factor
+#' @export
+#'
+#' @examples
+#' csf(mcap)
+
+csf <- function(mesh, L0, silent = FALSE) {
+  res <- Rvcg::vcgMeshres(mesh)$res[[1]]
+  if (missing(L0)){
+    L0 <- res
+    if (!silent){
+      message(paste("L0 is set to mesh resolution (", L0, ")", sep = ""))
+    }
+  }
+  if (L0 < res) {
+    if (!silent){
+      warning("L0 is smaller than mesh resolution")
+    }
+  }
+  m2 <- mesh
+  m2$vb[3,] <- 0
+  m2 <- Rvcg::vcgQEdecim(m2, edgeLength = L0, silent = T)
+  Rvcg::vcgArea(m2)
+}
