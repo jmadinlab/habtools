902749ad8cf8e5f6ee93c12834a6d695df5bc70c
Joshua Madin <jmadin@hawaii.edu>
2024-01-23 07:58:59 -1000

    Some updates to fd_diagnose

--- R/fd_diagnose.R
+++ R/fd_diagnose.R
@@ -18,7 +18,7 @@ fd_diagnose <- function(data, lvec, method, ...) {
   dta <- d[["data"]]
   dval <- d[["fd"]]
 
-  fdvec <- diff(log10(dta$h)) / diff(log10(dta$l))
+  f <- diff(log10(dta[,2])) / diff(log10(dta[,1]))
   if (method == "area") {
     f <- 2 - f
   }
@@ -31,10 +31,20 @@ fd_diagnose <- function(data, lvec, method, ...) {
 
   df <- data.frame(l = dta[,1][1:length(dta[,1])-1], fd = f)
 
-  plot(dta[,2] ~ dta[,1], xlab = colnames(dta)[1], ylab = colnames(dta)[2], log="xy", type="o")
+  plot(dta[,2] ~ dta[,1], xlab = colnames(dta)[1], ylab = colnames(dta)[2], log="xy", type="l", lty=2, col="grey", axes=FALSE, main=method)
+  axis(1)
+  axis(2, las=2)
+  points(dta[,2] ~ dta[,1])
+  text(midv(dta[,1]), midv(dta[,2]), round(f, 2))
   pred <- predict(lm(log10(dta[,2]) ~ log10(dta[,1])))
-  lines(log10(dta[,1]), pred, lty = 1, col = "red")
-
+  lines(dta[,1], 10^pred, lty = 1, col = "red")
+  if (method == "hvar") {
+    legend("bottomright", legend=c(paste0("D = ", round(dval, 2)), paste0("var = ", round(sd(f), 2))), bty="n")
+  } else {
+    legend("topright", legend=c(paste0("D = ", round(dval, 2)), paste0("var = ", round(sd(f), 2))), bty="n")
+  }
   return(list(df, D = unname(dval), var = sd(f)))
 
 }
+
+midv <- function(v) { v[-length(v)] + diff(v)/2 }
