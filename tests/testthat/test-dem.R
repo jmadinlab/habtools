7124465bde12674ef1f1bc3687a4538d3e268bfb
nschiett <nina.schiettekatte@gmail.com>
2024-01-30 14:57:30 -1000

    fix test

--- tests/testthat/test-dem.R
+++ tests/testthat/test-dem.R
@@ -1,5 +1,5 @@
 test_that("fd works", {
   expect_length(
-    fd(horseshoe, method = "area", x = -470, y = 1266, L = 2, lvec = c(0.125, 0.25, 0.5, 1, 2)),
+    fd(horseshoe, method = "area", lvec = c(0.125, 0.25, 0.5, 1, 2)),
     1)
 })
