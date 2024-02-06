04c509c2599f1657db20c2eb5ebf58bc55c5561f
nschiett <nina.schiettekatte@gmail.com>
2024-02-06 11:04:59 -1000

    avoid parallel = T in examples

--- R/hvar.R
+++ R/hvar.R
@@ -15,7 +15,6 @@
 #' @examples
 #'
 #' hvar(horseshoe, lvec = c(0.125, 0.25, 0.5, 1))
-#' hvar(horseshoe, lvec = c(0.125, 0.25, 0.5, 1), parallel=TRUE)
 
 
 hvar <- function(data, lvec=NULL, parallel = FALSE,
