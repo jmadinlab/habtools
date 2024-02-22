58f726ec5bc5b32bcd499e6c5b76d23e9ee21e14
Joshua Madin <jmadin@hawaii.edu>
2024-02-22 11:31:01 -1000

    New sample crop algorithm

--- R/dem_sample.R
+++ R/dem_sample.R
@@ -10,15 +10,39 @@
 #' dem <- dem_sample(horseshoe, L = 4)
 #' raster::plot(dem)
 
-dem_sample <- function(data, L) {
-  size <- L
-  mid <- mid_find(data)
-  sub <- dem_crop(data,
-                  x0 = mid$x_mid[1],
-                  y0 = mid$y_mid[1],
-                  L = raster::extent(data)[2] - raster::extent(data)[1] - size)
-  xy <- raster::rasterToPoints(sub)
-  xyi <- xy[sample(1:nrow(xy), size = 1),]
+dem_sample <- function(data, L, allow_NA=TRUE, plot=FALSE) {
 
-  dem_crop(data, x0 = xyi[1], y0 = xyi[2], L = size)
+  sub <- dem_runif(data, L)
+  while (sub$NAs != allow_NA) {
+    sub <- dem_runif(data, L)
+  }
+
+  dem_crop(data, x0 = sub$x0, y0 = sub$y0, L = L, plot=plot)
+
+  # size <- L
+  # mid <- mid_find(data)
+  # sub <- dem_crop(data,
+  #                 x0 = mid$x_mid[1],
+  #                 y0 = mid$y_mid[1],
+  #                 L = raster::extent(data)[2] - raster::extent(data)[1] - size)
+  # xy <- raster::rasterToPoints(sub)
+  # xyi <- xy[sample(1:nrow(xy), size = 1),]
+  #
+  # dem_crop(data, x0 = xyi[1], y0 = xyi[2], L = size, plot=plot)
+}
+
+
+dem_runif <- function(data, L) {
+  xmin <- raster::extent(data)[1] + L/2
+  xmax <- raster::extent(data)[2] - L/2
+  ymin <- raster::extent(data)[3] + L/2
+  ymax <- raster::extent(data)[4] - L/2
+
+  x0 <- (xmin + L/2) + runif(1) * (xmax - xmin)
+  y0 <- (ymin + L/2) + runif(1) * (ymax - ymin)
+
+  sub <- dem_crop(data, x0, y0, L)
+  NAs <- any(is.na(raster::values(sub)))
+
+  return(list(sub=sub, NAs=NAs, x0=x0, y0=y0))
 }
